a053fc300f2c08ccd5d8b772e34a0e8c31e5588d
Malachy <malachyfox1@gmail.com>
2025-01-06 13:58:02 +0000

    cleaned up testing

--- testing.py
+++ testing.py
@@ -23,41 +23,7 @@ def get_results(model,dataloader):
                 'preds': preds,
                 'ids':ids}
 
-def generate_table(labels, preds):
-    TP = 0
-    FP = 0
-    TN = 0
-    FN = 0
-
-    for label, pred in zip(labels,preds):
-        if label == 0.5:
-            continue
-        else:
-            if label == 1:
-                if pred > 0.5:
-                    TP += 1
-                else:
-                    FN += 1
-            else:
-                if pred > 0.5:
-                    FP += 1
-                else:
-                    TN += 1
-    return (TP, FP, TN, FN)
-
-def analyse_table(table):
-    TP, FP, TN, FN = table
-    sensitivity = TP / (TP + FN)
-    specificity = TN / (TN + FP)
-    balanced_accuracy = 0.5 * (sensitivity + specificity)
-    analysis = {'sensitivity': sensitivity,
-                'specificity': specificity,
-                'balanced_accuracy': balanced_accuracy}
-    return analysis
-
 def do_test(model,test_dataloader,hps):
     print('Testing...')
     results = get_results(model,test_dataloader)
-    # table = generate_table(results)
-    # analysis = analyse_table(results)
     return results
